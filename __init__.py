f46f66f62d888c39c23b829cff6ce7b461164eba
madisonj5 <55023096+madisonj5@users.noreply.github.com>
2021-10-11 22:13:11 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,17 +5,9 @@ import check50.c
 def exists():
     """hangman.c exists"""
     check50.exists("hangman.c")
+    check50.include("hangman.h")
     
 @check50.check(exists)
 def compiles():
     """hangman.c compiles"""
-#     check50.c.compile("hangman.c", lcs50=True)
-
-    hangman = open("hangman.c").read()
-    header = open("hangman.h").read()
-    
-    with open("hangman_file.c", "w") as f:
-        f.write(hangman)
-        f.write("\n")
-        f.write(header)
-    check50.c.compile("hangman_file.c", lcs50=True)
+    check50.c.compile("hangman.c", lcs50=True)
