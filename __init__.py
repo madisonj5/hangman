033e23eef749a768e74e22a6fca5db47d353dd2c
madisonj5 <55023096+madisonj5@users.noreply.github.com>
2021-10-11 22:02:59 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,3 +5,8 @@ import check50.c
 def exists():
     """hangman.c exists"""
     check50.exists("hangman.c")
+    
+@check50.check(exists)
+def compiles():
+    """hangman.c compiles"""
+    check50.c.compile("hangman.c", lcs50=True)
