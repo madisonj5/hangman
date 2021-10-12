6770044c53b2bcdf14527707202f9bda6e1f6682
madisonj5 <55023096+madisonj5@users.noreply.github.com>
2021-10-11 22:38:37 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -19,5 +19,5 @@ def test():
     @check50.check(compiles)
 def test():
     """testing"""
-    check50.run("./hangman a").stdin("a").stdout("a You guessed it!").exit(0)
+    check50.run("./hangman test").stdin("a")..stdin("a")..stdin("a").stdout("'_ _ _ _ Guess a letter: _ e _ _ Guess a letter: t e _ t Guess a letter: t e _ t Guess a letter: t e s t You guessed it!").exit(0)
 
